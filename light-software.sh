587d4b1fb99fe40607b9f01feedcd7d138910fc9
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2025-07-05 12:37:11 +0200

    y

--- light-software.sh
+++ light-software.sh
@@ -1,6 +1,7 @@
-# ARCO
+# ENDOS
 pkmgr="pacman -S --noconfirm --needed"
 
+sudo $pkmgr arc-gtk-theme-eos
 sudo $pkmgr bat
 sudo $pkmgr btop
 sudo $pkmgr eza
@@ -10,28 +11,27 @@ sudo $pkmgr ffmpeg
 sudo $pkmgr fish
 sudo $pkmgr fzf
 sudo $pkmgr git
+sudo $pkmgr glow
 sudo $pkmgr gparted
 sudo $pkmgr imagemagick
 sudo $pkmgr kbackup
 sudo $pkmgr micro
 sudo $pkmgr mpv
+sudo $pkmgr ncdu
+sudo $pkmgr nemo
 sudo $pkmgr neovim
+sudo $pkmgr newsboat
+sudo $pkmgr nsxiv
+sudo $pkmgr pavucontrol
 sudo $pkmgr ripgrep
+sudo $pkmgr scrot
+sudo $pkmgr sxhkd
 sudo $pkmgr tldr
 sudo $pkmgr tmux
-sudo $pkmgr yt-dlp
-sudo $pkmgr zoxide
-sudo $pkmgr ncdu
-sudo $pkmgr scrot
-sudo $pkmgr pavucontrol
-sudo $pkmgr zathura zathura-cb zathura-ps zathura-pdf-mupdf zathura-djvu
-sudo $pkmgr nsxiv
-sudo $pkmgr newsboat
-sudo $pkmgr glow
-sudo $pkmgr nemo
 sudo $pkmgr virtualbox-utils-guest
-sudo $pkmgr arc-gtk-theme-eos
-sudo $pkmgr sxhkd
+sudo $pkmgr yt-dlp
+sudo $pkmgr zathura zathura-cb zathura-ps zathura-pdf-mupdf zathura-djvu
+sudo $pkmgr zoxide
 
 yay -S --noconfirm --needed ungoogled-chromium-bin
 yay -S --noconfirm --needed paru
