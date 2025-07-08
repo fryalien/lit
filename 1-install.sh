307b9c82dedb416c2f9b952079412a228170c410
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2025-07-08 12:52:32 +0200

    y

--- 1-install.sh
+++ 1-install.sh
@@ -4,10 +4,8 @@
 
 cp -r wallpapers ~/.local/
 
-mkdir -p ~/.config/fish/
+sudo chsh -s /usr/bin/fish y
 
 cp config.fish ~/.config/fish/
-
-sudo chsh -s /usr/bin/fish y
 
 source ~/.config/fish/config.fish
